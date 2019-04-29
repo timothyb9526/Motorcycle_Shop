3202ca22729a4182602d202f5f7dbbea457283a3
timothyb9526 <tbowling@basecampcodingacademy.org>
2019-04-29 14:31:30 -0500

    fixed styling issues

--- src/main/resources/static/js/javascript.js
+++ src/main/resources/static/js/javascript.js
@@ -1,10 +1,10 @@
 var x = document.getElementById("mySelect");
 var y = document.getElementById("brand");
 var result = document.getElementById("result");
-var honda = ["Touring", "Adventure", "Cruiser", "Standard", "Sport", "Neo-Sports cafe", "MiniMoto"]
-var kawasaki = ["Sport", "Naked", "Adventure", "Touring", "Cruiser", "off-Road"]
-var harley = ["Street", "Sportster", "Softail", "Touring", "Trike", "CVO"]
-var yamaha = ["Motorcross", "Cross country", "Trail", "Sport", "Naked", "Heritage", "Touring"]
+var honda = ["Africa Twin", "CB", "CBR", "CRF", "Gold Wing", "Interceptor", "RC", "V4", "VFR", "XR", "Integra", "Monkey", "X-ADV", "599", "919", "CBF", "CBX", "CR", "CTX", "DN 01", "Yuga", "EVO 6", "Fury", "Grom", "Interstate", "Magna", "Montesa Cota", "NC", "Nighthawk", "NM4", "NT", "Rebel", "Rune", "Sabre", "Shadow", "Stateline", "Super Hawk", "Transalp", "VTR", "VTX"];
+var kawasaki = ["ER 6N", "KLX", "KX", "Ninja", "Versys", "Vulcan", "Z", "W", "Concours", "D-Tracker", "Eliminator", "KLR", "ZZR"];
+var harley = ["CVO", "Dyna", "Fat Boy", "Road Glide", "Road King", "Roadster", "Softail", "Sportster", "Street", "Touring", "Trike", "V-Rod", "Breakout", "FXDR", "Iron 1200", "Livewire", "Electra Glide", "Fat Bob", "Forty-Eight", "Iron 883", "Low Rider", "Nightster", "Rod Special", "Seventy-Two", "Street Bob", "Street  Glide", "Street Glide Special", "SuperLow", "TriGlide", "VRSC", "XR"];
+var yamaha = ["Bolt", "Fazer", "FZ", "MT", "Star", "V-Max", "V-star", "XT", "YZ", "YZF", "YZF-R1", "FJ", "FJR", "FZR", "FZ5", "Niken", "PW", "Raider", "Roadliner", "Road Star", "Royal Star", "SCR", "SS", "Stratoliner", "Stryker", "SZ", "TDM", "Tenere", "Tracer", "TT", "TW", "Virago", "Warrior", "WR", "XJ", "XSR", "YBR", "SR"];
 makeDropdown(honda);
  y.oninput = function() {
     if (y.value.toLowerCase() == "honda") {
