342a4e20566127cdd986a244dbf9ee5a38ff4ede
timothyb9526 <tbowling@basecampcodingacademy.org>
2019-04-29 11:15:51 -0500

    fixed styling issues

--- src/main/java/com/timothy/Shop/model/Bike.java
+++ src/main/java/com/timothy/Shop/model/Bike.java
@@ -1,17 +1,15 @@
 package com.timothy.Shop.model;
-
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-
 import javax.validation.constraints.NotNull;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 @Data
 @RequiredArgsConstructor
 public class Bike {
-
     public UUID id;
     @NotNull
     public String brand;
@@ -43,15 +41,8 @@ public class Bike {
     public String email;
     @NotNull
     public String phone;
-
-
-
-
     public Bike(UUID id, String brand, String type, String image, Integer price, String name, String description, String email, String phone, String date, String year, String city, String state, String color, int mileage, String condition) {
-
-        String pattern = "dd-MM-yyyy HH:mm:ss";
-        String dateInString = new SimpleDateFormat(pattern).format(new Date());
-
+        String number = phone.replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1) $2-$3");
         this.id = id;
         this.brand = brand.toLowerCase();
         this.type = type;
@@ -60,7 +51,7 @@ public class Bike {
         this.name = name;
         this.description = description;
         this.email = email;
-        this.phone = phone;
+        this.phone = number;
         this.date = date;
         this.year = year;
         this.city = city;
@@ -69,9 +60,4 @@ public class Bike {
         this.mileage = mileage;
         this.condition = condition;
     }
-
-
-
-
-
 }
--- src/main/resources/static/js/javascript.js
+++ src/main/resources/static/js/javascript.js
@@ -5,39 +5,26 @@ var honda = ["Touring", "Adventure", "Cruiser", "Standard", "Sport", "Neo-Sports
 var kawasaki = ["Sport", "Naked", "Adventure", "Touring", "Cruiser", "off-Road"]
 var harley = ["Street", "Sportster", "Softail", "Touring", "Trike", "CVO"]
 var yamaha = ["Motorcross", "Cross country", "Trail", "Sport", "Naked", "Heritage", "Touring"]
-
 makeDropdown(honda);
-
  y.oninput = function() {
-
     if (y.value.toLowerCase() == "honda") {
         x.innerHTML = "";
         makeDropdown(honda)
-
     } else if (y.value.toLowerCase() == "kawasaki") {
         x.innerHTML = "";
         makeDropdown(kawasaki)
-
     } else if (y.value.toLowerCase() == "harley") {
           x.innerHTML = "";
           makeDropdown(harley)
-
     } else if (y.value.toLowerCase() == "yamaha")  {
         x.innerHTML = "";
         makeDropdown(yamaha)
-
     }
-
   };
-
 function makeDropdown(list) {
-
     for (var i = 0; i < list.length; i++) {
         var option = document.createElement("option");
         option.text = list[i];
         x.add(option);
-
     }
-
 }
-
